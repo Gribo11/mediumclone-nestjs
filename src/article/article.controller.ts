c63c42c8b78a268234d7dfdd46bd9cc682f65628
S.Kuzmenko <s.kuzmenko@sense.pro>
2022-08-05 16:19:26 +0300

    delete article

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,15 @@
 import { User } from "@app/user/decorators/user.decoratir";
 import { AuthGuard } from "@app/user/guards/guard";
 import { UserEntity } from "@app/user/user.entity";
-import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from "@nestjs/common";
 import { ArticleService } from "./article.service";
 import { CreateArticleDto } from "./dto/createArticle.dto";
 import { ArticleResponseInterface } from "./types/articleResponse.interface";
@@ -29,5 +37,14 @@ export class ArticleContorller {
   ): Promise<ArticleResponseInterface> {
     const article = await this.articleService.findBySlug(slug);
     return this.articleService.buildArticleResponse(article);
+  }
+
+  @Delete(":slug")
+  @UseGuards(AuthGuard)
+  async deleteArticle(
+    @User("id") currentUserId: number,
+    @Param("slug") slug: string
+  ) {
+    return await this.articleService.deleteArticle(slug, currentUserId);
   }
 }
